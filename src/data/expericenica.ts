0a4b042c6d9fd9e1c173ebbd86d450738befb0ce
Anna <annaclararussoluca@gmail.com>
2025-12-11 15:08:16 -0300

    feat: criando data para informações de experiência

---
+++ src/data/expericenica.ts
@@ -0,0 +1,6 @@
+import type { Experiencia } from "../types/types";
+
+export const experiences: Experiencia[] = [
+    { data: "Abril de 2025 — Agosto de 2025", titulo: "Jovem Aprendiz", descricao: "Porto · Atendimento ao cliente - Cartão de crédito N1." },
+    { data: "Janeiro de 2025 — Dezembro de 2026", titulo: "Formação acadêmica", descricao: "FIAP · Análise e Desenvolvimento de Sistemas." },
+];
