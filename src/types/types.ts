e97e730c2c640b2cdf698a18390deb050b29bb01
Anna <annaclararussoluca@gmail.com>
2025-12-11 13:41:22 -0300

    feat: criando types para CardProjeto

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,10 @@ export interface ThemeContextType {
   theme: 'light' | 'dark';
   toggleTheme: () => void;
 }
+
+export interface Project {
+  title: string;
+  description: string;
+  tech: string[];
+  imagem: string;
+}
