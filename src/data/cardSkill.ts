22ffba1f29ae873f6589cced052e18efb0bd1ceb
Anna <annaclararussoluca@gmail.com>
2025-12-16 14:43:14 -0300

    feat: criando data para as informações dos cards de skills

---
+++ src/data/cardSkill.ts
@@ -0,0 +1,58 @@
+const skills = [
+  {
+    titulo: "Linguagens",
+    icone: "{}",
+    skills: [
+      {
+        nome: "Java",
+        tags: ["POO", "Collections", "Streams"]
+      },
+      {
+        nome: "JavaScript",
+        tags: ["Async/Await", "DOM"]
+      },
+      {
+        nome: "TypeScript",
+        tags: ["Tipagem", "Interfaces", "Types"]
+      }
+    ]
+  },
+  {
+    titulo: "Back-end",
+    icone: "⚙️",
+    skills: [
+      {
+        nome: "Spring Boot",
+        tags: ["APIs REST", "JPA", "Validações"]
+      },
+      {
+        nome: "Quarkus",
+        tags: ["Config", "Dev Mode"]
+      }
+    ]
+  },
+  {
+    titulo: "Front-end",
+    icone: "🎨",
+    skills: [
+      {
+        nome: "React",
+        tags: ["Hooks", "Componentização", "Props"]
+      },
+      {
+        nome: "Tailwind CSS",
+        tags: ["Responsividade", "Dark Mode"]
+      }
+    ]
+  },
+  {
+    titulo: "Banco de Dados",
+    icone: "🗄️",
+    skills: [
+      {
+        nome: "SQL",
+        tags: ["Modelagem", "DML", "DDL", "DRS"]
+      }
+    ]
+  }
+];
